c692a46949ca17b4368214e120b2e8d8fa195ac6
nat3 <nat3Github>
2025-05-22 18:15:07 +0200

    ok

--- build.zig
+++ build.zig
@@ -1,8 +1,31 @@
-// SPDX-License-Identifier: MPL-2.0
-//   Copyright © 2024 Chris Marchesi
 const std = @import("std");
+const update = @import("update.zig");
+const GitDependency = update.GitDependency;
+fn update_step(step: *std.Build.Step, _: std.Build.Step.MakeOptions) !void {
+    const deps = &.{
+        // GitDependency{
+        //     // z2d vancluever
+        //     .url = "https://github.com/vancluever/z2d",
+        //     .branch = "main",
+        // },
+        GitDependency{
+            // z2d fixed fork
+            .url = "https://github.com/nat3Github/zig-lib-z2d-dev-fork",
+            .branch = "main",
+        },
+        GitDependency{
+            // zig-xml
+            .url = "https://github.com/ianprime0509/zig-xml",
+            .branch = "main",
+        },
+    };
+    try update.update_dependency(step.owner.allocator, deps);
+}
 
 pub fn build(b: *std.Build) void {
+    const step = b.step("update", "update git dependencies");
+    step.makeFn = update_step;
+
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const this_module = b.addModule("svg2tvg", .{
