b78d7a6e8f91d5fedeb79c3bfb1fe03786e6baac
Morgan Moore <mdm5995@gmail.com>
2026-01-11 16:37:16 -0500

    Update intToEnum -> enums.fromInt handle null instead of error

--- src/tinyvg/parsing.zig
+++ src/tinyvg/parsing.zig
@@ -301,7 +301,8 @@ pub fn Parser() type {
             if (self.end_of_document)
                 return null;
             const command_byte = try self.reader.takeByte();
-            const primary_style_type = std.meta.intToEnum(tvg.StyleType, @as(u2, @truncate(command_byte >> 6))) catch return error.InvalidData;
+            const primary_style_type = std.enums.fromInt(tvg.StyleType, @as(u2, @truncate(command_byte >> 6))) orelse return error.InvalidData;
+
             const command: tvg.Command = @enumFromInt(@as(u6, @truncate(command_byte)));
 
             return switch (command) {
